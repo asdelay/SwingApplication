3f9c97b7d3ee653f1700be2a67b133bb283ba7ac
asdelay <oleksandrptuha1@gmail.com>
2022-04-17 20:16:08 +0300

    Second Tutorial

--- src/App.java
+++ src/App.java
@@ -1,19 +1,12 @@
 import javax.swing.*;
-import static javax.swing.SwingUtilities.*;
+import javax.swing.SwingUtilities;
 
 public class App {
     public static void main(String[] args) {
-//        JFrame frame = new JFrame("My First GUI Application");
-//        frame.setSize(600, 400);
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.setVisible(true);
-        invokeLater(new Runnable(){     //в метод передаем параметр имлементирующий анон обьект класса
+        SwingUtilities.invokeLater(new Runnable(){     //в метод передаем параметр имлементирующий анон обьект класса
             @Override       //  интерфейс
             public void run() {
-                JFrame frame = new JFrame("My First GUI Application");
-                frame.setSize(600, 400);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setVisible(true);
+                new MyFrame();
             }
         });
     }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,4 +1,24 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MyFrame extends JFrame {
+    private JTextArea textArea;
+    private JButton btn;
+
+    MyFrame(){
+        super("My GUI application");
+
+        //adding layout and components
+        setLayout(new BorderLayout());
+
+        textArea = new JTextArea();
+        btn = new JButton("Actions");
+
+        add(textArea, BorderLayout.CENTER);
+        add(btn, BorderLayout.SOUTH);
+
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
